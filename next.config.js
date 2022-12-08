0f94d97c2dc796d971f0566bdb97f794ab9dfc2b
Bernardo D'Addario <94189575+Khoderb@users.noreply.github.com>
2022-12-08 07:57:23 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env : {
-      backendURL : 'https://node-send-reactjs.herokuapp.com',
+      backendURL : 'https://node-sendhttps-dashboard-render-com.onrender.com/',
       frontURL : 'https://next-send.netlify.app'  
   }
 }
